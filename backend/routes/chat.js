e93fd4293c04e5b7fc7bf7d6d82f0a7b934eee4e
parksomins <86812172+parksomins@users.noreply.github.com>
2022-01-02 19:08:30 +0900

    Update chat.js

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const app = express();
+
 const mysql = require('mysql');
 const config = require('../config/config.json');
+
+const cors = require('cors');
+
+
 const bodyParser = require('body-parser');
 const pool = mysql.createPool(config);
+
 const router = express.Router()
-router.use(bodyParser.urlencoded({ extended: false }))
-
-
-
+router.use(cors());
+router.use(express.static('views'));
 
 module.exports = router
